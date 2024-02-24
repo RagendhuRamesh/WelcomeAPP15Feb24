f8cc277de7911990f5c1927d04d2c12a920ebbd2
RagendhuRamesh <ragendhuram@gmail.com>
2024-02-24 11:14:16 +0530

    Final commit

---
+++ SignIn.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class SignIn
+    {
+        public string Auhtenticate(string userName, string password)
+        {
+            string msg;
+            if(string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                msg = "Use name and password required";
+            }
+            else
+            if ((userName == "admin") && (password == "admin@1256"))
+            {
+                msg = "Authentication Pass";
+            }
+            else
+            {
+                msg = "Authentication Fail";
+            }
+            return msg;
+        }
+    }
+}
